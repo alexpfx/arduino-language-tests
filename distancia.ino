c80dd64355c8c9af7d4101bb0f46e1a18e975e0e
alexpfx <alexpfba@gmail.com>
2015-08-29 00:41:52 -0300

    utilizando library.

--- distancia.ino
+++ distancia.ino
@@ -1,20 +1,31 @@
-#include <Arduino.h>
-const float SOUND_SPEED = 340.29; //metros por segundo.
+#include <NewPing.h>
 
+
+const float SOUND_SPEED = 340.29; //metros por segundo.
+//lembrete : conectar pino GND antes do VCC para evitar erros segundo o fabricante.
 int trigPin = 9;
 int echoPin = 8;
 long last = 0;
 long millis_wait = 1000;
 float distance;
 
+NewPing sonar(trigPin, echoPin, 400);
+
+
 void setup() {
     Serial.begin(9600);
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+
     digitalWrite(trigPin, LOW);
+    digitalRead (echoPin);
+
+
 }
 
 void loop() {
+
+
     if (Serial.available()) {
         char c = Serial.read();
         if (((millis() - last) > millis_wait)) {
@@ -27,6 +38,7 @@ void loop() {
 
 boolean interpret(char c) {
     if (c == 't') {
+
         return printDistance();
     }
     return false;
@@ -34,13 +46,8 @@ boolean interpret(char c) {
 
 
 boolean printDistance (){
-    digitalWrite (trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite (trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    distance = pulseIn(echoPin, HIGH) / 58 ; // cm
-    Serial.println(distance);
+    int uS = sonar.ping();
+    Serial.println(uS / US_ROUNDTRIP_CM);
 
     return true;
 }
